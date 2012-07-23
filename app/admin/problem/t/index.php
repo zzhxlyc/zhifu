4dc6f66776d3bc56127796ed5469176c566e60bb
hpp <jessiehan1989@gmail.com>
2012-07-23 16:45:54 +0800

    难题index UI

--- app/admin/problem/t/index.php
+++ app/admin/problem/t/index.php
@@ -3,3 +3,40 @@
 		print_r($l);
 	}
 ?>
+
+<table class="normal-table" cellspacing="0" cellpadding="0">
+	<tr class="top">
+		<td>选择</td>
+		<td>难题名称</td>
+		<td>发布人</td>
+		<td>日期</td>
+		<td>类型</td>
+		<td>状态</td>
+		<td>金额</td>
+		<td>操作</td>
+	</tr>
+	<tr>
+		<td><input type="checkbox" /></td>
+		<td><a href="">难题名称</a></td>
+		<td>发布人</td>
+		<td>日期</td>
+		<td>类型</td>
+		<td>状态</td>
+		<td>金额</td>
+		<td class="operate"><a href="#">编辑</a><a href="#">删除</a></td>
+	</tr>
+	<tr class="even">
+		<td><input type="checkbox" /></td>
+		<td><a href="">难题名称</a></td>
+		<td>发布人</td>
+		<td>日期</td>
+		<td>类型</td>
+		<td>状态</td>
+		<td>金额</td>
+		<td class="operate"><a href="#">编辑</a><a href="#">删除</a></td>
+	</tr>
+</table>
+
+<a href="#" class="batch-del">批量删除</a>
+
+
