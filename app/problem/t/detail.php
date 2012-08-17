dc44b75a7b5836244a3648993ff5be6ef94b1c8d
hpp <jessiehan1989@gmail.com>
2012-08-17 16:40:36 +0800

    单个难题页面

--- app/problem/t/detail.php
+++ app/problem/t/detail.php
@@ -1,42 +1,44 @@
 <div class="sidebar">
-	<div class="pic">
-		<img src="<?php img($Expert->image)?>" alt="<?php echo $Expert->name?>" width="200" height="150"/>
-	</div>
+
 	<div class="detail-profile">
-		<p>姓名：<?php echo $Expert->name?></p>
-		<p>所在单位：<?php echo $Expert->workplace?></p>
-		<p>职称：<?php echo $Expert->job?></p>
-		<p>参与项目金额：<?php output_money($Expert->budget)?></p>
-		<p>综合评价：<?php echo $Expert->get_rate()?>星</p>
+		<img src="http://zjuhpp.com/demo/wp-content/uploads/2012/02/hero.jpg" alt="" width="180" height="150"/>
+		<p>难题名称</p>
+		<p>金额<span class="price">50w</span></p>
+		<p></p>
 	</div><!--end for detail-profile-->
 	
 	<div class="side-section">
-		<div class="title"><h4>领域标签</h4></div>
+		<div class="title">发起者</div>
 		<div class="content">
-			<?php foreach($tags as $tag){?>
-			<span class="item"><?php echo $tag->name?></span>
-			<?php }?>
+			<p>上海大总管</p>
+			
 		</div>
+		
 	</div><!--end for tag-->
+	
+	<div class="side-section">
+		<div class="title">领域标签</div>
+		<div class="content">
+			<span class="item">自动化</span>
+			<span class="item">自动化</span>
+			<span class="item">自动化</span>
+		</div>
+	</div>
 	
 	
 </div><!--end for sidebar-->
 
 <div class="main-content">
 	<div class="section">
-		<h3>我的专利</h3>
-		<div class="list clearfix">
-			<?php 
-				foreach($patents as $patent){
-			?>
-			<div class="item">
-				<div class="pic">
-					<img src="<?php img($patent->image)?>" alt="" width="100" height="100"/>
-				</div>
-				<div class="des"><?php echo $patent->title?></div>
-			</div><!--end for item-->
-			<?php }?>
-		</div><!--end for list-->
+		<h3>项目状态<a href="#" class="join-btn btn">我要竞标</a></h3>
+		<div class="content status clearfix">
+			<div class="status-item current">发布南图</div>
+			<div class="status-item">竞标中</div>
+			<div class="status-item">付款</div>
+			<div class="status-item last">交付互评</div>
+			
+		</div>
+		
 		
 		
 	</div><!--end for section-->
@@ -44,18 +46,32 @@
 
 
 	<div class="section">
-		<h3>我参与的项目</h3>
-		<div class="list clearfix">
-			<?php 
-				foreach($problems as $problem){
-			?>
-			<div class="item">
+		<h3>竞标专家(2)</h3>
+		<div class="content line-list">
+			<div class="item clearfix">
 				<div class="pic">
-					<img src="<?php img($problem->image)?>" alt="" width="100" height="100"/>
+					<img src="http://zjuhpp.com/demo/wp-content/uploads/2012/02/hero.jpg" alt="" width="100" height="100"/>
+					<span class="name">名字</span>
 				</div>
-				<div class="des"><?php echo $problem->title?></div>
+				<div class="des">描述描述描述描述描述描述描述</div>
 			</div><!--end for item-->
-			<?php }?>
+		
+			
+			<div class="item clearfix">
+				<div class="pic">
+					<img src="http://zjuhpp.com/demo/wp-content/uploads/2012/02/hero.jpg" alt="" width="100" height="100"/>
+					<span class="name">名字</span>
+					
+				</div>
+				<div class="des">描述</div>
+			</div><!--end for item-->
+			
+		
+			
+		
+			
+			
+						
 		</div><!--end for list-->
 		
 		
