3fcee9c24d738d373aad74531e4dbf6e394f071e
hpp <jessiehan1989@gmail.com>
2012-10-02 23:01:20 +0800

    首页

---
+++ app/zhifu/t/index.php
@@ -0,0 +1,265 @@
+<div class="index">
+<div class="left-content">
+
+	<div class="idea-wrapper list clearfix">
+		<h3>创意悬赏</h3>
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="content">
+				<div class="title"><a href="">第一个创意悬赏</a></div>
+				<div class="des">
+					<p>9月30日发布，10月7日截止</p>
+					<p>参与人数：9人</p>
+				</div>
+			</div>
+		</div><!--end for item-->
+		
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="content">
+				<div class="title"><a href="">第一个创意悬赏</a></div>
+				<div class="des">
+					<p>9月30日发布，10月7日截止</p>
+					<p>参与人数：9人</p>
+				</div>
+			</div>
+		</div><!--end for item-->
+		
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="content">
+				<div class="title"><a href="">第一个创意悬赏</a></div>
+				<div class="des">
+					<p>9月30日发布，10月7日截止</p>
+					<p>参与人数：9人</p>
+				</div>
+			</div>
+		</div><!--end for item-->
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="content">
+				<div class="title"><a href="">第一个创意悬赏</a></div>
+				<div class="des">
+					<p>9月30日发布，10月7日截止</p>
+					<p>参与人数：9人</p>
+				</div>
+			</div>
+		</div><!--end for item-->
+		
+		<span class="more"><a href="">查看更多</a></span>
+	</div>
+	
+	<div class="problem-wrapper list">
+		<h3>技术难题</h3> 
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="content">
+				<div class="title"><a href="">第一个nanti </a></div>
+				<div class="des">
+					<p>所属领域：计算机</p>
+					<p>描述描述</p>
+				</div>
+			</div>
+		</div><!--end for item-->
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="content">
+				<div class="title"><a href="">第一个nanti </a></div>
+				<div class="des">
+					<p>所属领域：计算机</p>
+					<p>描述描述</p>
+				</div>
+			</div>
+		</div><!--end for item-->
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="content">
+				<div class="title"><a href="">第一个nanti </a></div>
+				<div class="des">
+					<p>所属领域：计算机</p>
+					<p>描述描述</p>
+				</div>
+			</div>
+		</div><!--end for item-->
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="content">
+				<div class="title"><a href="">第一个nanti </a></div>
+				<div class="des">
+					<p>所属领域：计算机</p>
+					<p>描述描述</p>
+				</div>
+			</div>
+		</div><!--end for item-->
+	</div>
+	
+	
+
+</div>	<!--end for left-content-->
+<div class="right-content">
+	<div class="video-wrapper list ">
+		<h3>精彩视频</h3>
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="title">
+				<a href="">比哦啊他</a>
+			</div>
+		</div><!--end for item-->
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="title">
+				<a href="">比哦啊他</a>
+			</div>
+		</div><!--end for item-->
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="title">
+				<a href="">比哦啊他</a>
+			</div>
+		</div><!--end for item-->
+		<div class="item clearfix">
+			<div class="pic">
+				<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+			</div>
+			<div class="title">
+				<a href="">比哦啊他</a>
+			</div>
+		</div><!--end for item-->
+	</div>
+	
+</div><!--end for right-content-->
+<div class="clear"></div>
+<div class="job-wrapper job list">
+	<h3>兼职顾问</h3>
+	<table>
+		<tr class="top">
+			<th>类型</th>
+			<th>职位名称</th>
+			<th>招聘人数</th>
+			<th>发布者</th>
+			<th>发布时间</th>				
+		</tr>
+		<tr>
+			<td>aa</td>
+			<td>aa</td>
+			<td>ss</td>
+			<td>ss</td>
+			<td>ss</td>
+		</tr>
+		<tr>
+			<td>aa</td>
+			<td>aa</td>
+			<td>ss</td>
+			<td>ss</td>
+			<td>ss</td>
+		</tr>
+		<tr>
+			<td>aa</td>
+			<td>aa</td>
+			<td>ss</td>
+			<td>ss</td>
+			<td>ss</td>
+		</tr>
+		<tr>
+			<td>aa</td>
+			<td>aa</td>
+			<td>ss</td>
+			<td>ss</td>
+			<td>ss</td>
+		</tr>		
+	</table>
+</div>
+	
+<div class="patent-wrapper list clearfix">
+	<h3>科技成果</h3>
+	<div class="item">
+		<span class="title">名称</span><span class="cat">所在行业</span>
+	</div>
+	<div class="item">
+		<span class="title">名称</span><span class="cat">所在行业</span>
+	</div>
+	<div class="item">
+		<span class="title">名称</span><span class="cat">所在行业</span>
+	</div>
+	
+	<div class="item">
+		<span class="title">名称</span><span class="cat">所在行业</span>
+	</div>
+	<div class="item">
+		<span class="title">名称</span><span class="cat">所在行业</span>
+	</div>
+	<div class="item">
+		<span class="title">名称</span><span class="cat">所在行业</span>
+	</div>
+	
+	
+	
+	
+	
+</div><!--end for patent-wrapper-->
+
+
+<div class="case-wrapper list clearfix">
+	<h3>案例展示</h3>
+	<div class="item clearfix">
+		<div class="pic">
+			<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+		</div>
+		<div class="content">
+			<div class="title"><a href="">xxxxx</a></div>
+			<div class="des">
+				<p>9月30日发布，10月7日截止</p>
+				<p>参与人数：9人</p>
+			</div>
+		</div>
+	</div><!--end for item-->
+	<div class="item clearfix">
+		<div class="pic">
+			<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+		</div>
+		<div class="content">
+			<div class="title"><a href="">xxxxx</a></div>
+			<div class="des">
+				<p>9月30日发布，10月7日截止</p>
+				<p>参与人数：9人</p>
+			</div>
+		</div>
+	</div><!--end for item-->
+	<div class="item clearfix">
+		<div class="pic">
+			<img src="http://localhost/zhifu/images/default.jpg" width="100" height="75" alt="第一个创意悬赏">			
+		</div>
+		<div class="content">
+			<div class="title"><a href="">xxxxx</a></div>
+			<div class="des">
+				<p>9月30日发布，10月7日截止</p>
+				<p>参与人数：9人</p>
+			</div>
+		</div>
+	</div><!--end for item-->
+	
+</div><!--end for case-wrapper-->
+	
+</div>
