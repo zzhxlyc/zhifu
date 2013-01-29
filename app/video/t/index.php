e58bf6e1ad79495e4a9a0d9f30439fdac96f8dbe
hpp <jessiehan1989@gmail.com>
2013-01-29 11:23:04 +0800

    video

--- app/video/t/index.php
+++ app/video/t/index.php
@@ -6,121 +6,126 @@
 	<a href="http://localhost/zhifu/video/add" class="fr btn">上传视频</a>
 </div>
 
-<div class="section left-video-section fl">
-	<h3>技术推广视频</h3>
-	<div class="list clearfix">
-		<div class="video-item">
-			<a href="#"><img src="" alt="" width="200" height="200" /></a>
-			<div class="title">描描述描述描述描述描述描述描述述</div>
+<div class="video-left">
+	<div class="section left-video-section">
+		<h3>技术推广视频</h3>
+		<div class="list clearfix">
+			<div class="video-item">
+				<a href="#"><img src="" alt="" width="200" height="200" /></a>
+				<div class="title">描描述描述描述描述描述描述描述述</div>
+			</div>
+
 		</div>
-		
-	</div>
+
+
+	</div><!--end for left-video-section-->
 	
+	<div class="section left-video-section">
+		<h3>访谈节目视频</h3>
+		<div class="list clearfix">
+			<div class="video-item">
+				<a href="#"><img src="" alt="" width="200" height="200" /></a>
+				<div class="title">描描述描述描述描述描述描述描述述</div>
+			</div>
+
+		</div>
+
+
+	</div><!--end for left-video-section-->
+
+	<div class="section left-video-section">
+		<h3>加工制造视频</h3>
+		<div class="list clearfix">
+			<div class="video-item">
+				<a href="#"><img src="" alt="" width="200" height="200" /></a>
+				<div class="title">描描述描述描述描述描述描述描述述</div>
+			</div>
+
+		</div>
+
+
+	</div><!--end for left-video-section-->
+	
+	<div class="section left-video-section fl">
+		<h3>探索发现视频</h3>
+		<div class="list clearfix">
+			<div class="video-item">
+				<a href="#"><img src="" alt="" width="200" height="200" /></a>
+				<div class="title">描描述描述描述描述描述描述描述述</div>
+			</div>
+
+		</div>
+
+
+	</div><!--end for left-video-section-->
+	
+	<div class="section left-video-section fl">
+		<h3>科技视频</h3>
+		<div class="list clearfix">
+			<div class="video-item">
+				<a href="#"><img src="" alt="" width="200" height="200" /></a>
+				<div class="title">描描述描述描述描述描述描述描述述</div>
+			</div>
+
+		</div>
+
+
+	</div><!--end for left-video-section-->
+
 	
 </div>
 
-<div class="section right-video-section fr">
-	<h3>精彩推荐</h3>
-	<div class="list clearfix">
-		<div class="video-item">
-			<a href="#"><img src="" alt="" width="130" height="130" /></a>
-			<div class="title">描描述描述描述描述描述描述描述述</div>
-		</div>
-		
-	</div>
-</div>
 
-<div class="clear"></div>
-
-<div class="section left-video-section fl">
-	<h3>访谈节目视频</h3>
-	<div class="list clearfix">
-		<div class="video-item">
-			<a href="#"><img src="" alt="" width="200" height="200" /></a>
-			<div class="title">描描述描述描述描述描述描述描述述</div>
-		</div>
-		
-	</div>
+<div class="video-right">
 	
-	
-</div>
+	<div class="section right-video-section fr">
+		<h3>精彩推荐</h3>
+		<div class="list clearfix">
+			<div class="video-item">
+				<a href="#"><img src="" alt="" width="130" height="130" /></a>
+				<div class="title">描描述描述描述描述描述描述描述述</div>
+			</div>
 
-<div class="section right-video-section fr">
-	<h3>最新视频</h3>
-	<div class="list clearfix">
-		<div class="video-item">
-			<a href="#"><img src="" alt="" width="130" height="130" /></a>
-			<div class="title">描描述描述描述描述描述描述描述述</div>
 		</div>
-		
 	</div>
-</div>
 
-<div class="clear"></div>
+	<div class="section right-video-section fr">
+		<h3>最新视频</h3>
+		<div class="list clearfix">
+			<div class="video-item">
+				<a href="#"><img src="" alt="" width="130" height="130" /></a>
+				<div class="title">描描述描述描述描述描述描述描述述</div>
+			</div>
 
-
-<div class="section left-video-section fl">
-	<h3>加工制造视频</h3>
-	<div class="list clearfix">
-		<div class="video-item">
-			<a href="#"><img src="" alt="" width="200" height="200" /></a>
-			<div class="title">描描述描述描述描述描述描述描述述</div>
 		</div>
-		
 	</div>
 	
 	
-</div>
+	
+	<div class="section video-cat right-video-section fr">
+		<h3>视频分类</h3>
+		<div class="list clearfix">
+			<span class="cat-item">技术推广</span> 
+			<div class="subcat">
+				<a href="#">电子信息</a>
+				<a href="#">电子信息</a>
+				<a href="#">电子信息</a><a href="#">电子信息</a>
+				<a href="#">电子信息</a>
+				<a href="#">电子信息</a>
 
-<div class="section video-cat right-video-section fr">
-	<h3>视频分类</h3>
-	<div class="list clearfix">
-		<span class="cat-item">技术推广</span> 
-		<div class="subcat">
-			<a href="#">电子信息</a>
-			<a href="#">电子信息</a>
-			<a href="#">电子信息</a><a href="#">电子信息</a>
-			<a href="#">电子信息</a>
-			<a href="#">电子信息</a>
-			
+			</div>
+			<span class="cat-item">技术推广</span> 
+			<div class="subcat">
+				<a href="#">电子信息</a>
+				<a href="#">电子信息</a>
+				<a href="#">电子信息</a>
+			</div>
+
 		</div>
-		<span class="cat-item">技术推广</span> 
-		<div class="subcat">
-			<a href="#">电子信息</a>
-			<a href="#">电子信息</a>
-			<a href="#">电子信息</a>
-		</div>
-		
 	</div>
 </div>
 
-<div class="clear"></div>
 
-
-<div class="section left-video-section fl">
-	<h3>探索发现视频</h3>
-	<div class="list clearfix">
-		<div class="video-item">
-			<a href="#"><img src="" alt="" width="200" height="200" /></a>
-			<div class="title">描描述描述描述描述描述描述描述述</div>
-		</div>
-		
-	</div>
-	
-	
-</div>
-<div class="section left-video-section fl">
-	<h3>科技视频</h3>
-	<div class="list clearfix">
-		<div class="video-item">
-			<a href="#"><img src="" alt="" width="200" height="200" /></a>
-			<div class="title">描描述描述描述描述描述描述描述述</div>
-		</div>
-		
-	</div>
-	
-	
-</div>
 
 
 
